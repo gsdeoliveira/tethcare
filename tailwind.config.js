48c471b15e248194d21766382fdea1eec7f0a5ba
gsdeoliveira <gabriielsousa2002@gmail.com>
2023-12-15 20:38:42 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+      colors: {
+        bluePrimary: '#00A0D2',
+        cyanPrimary: '#00D1BF',
+        darkGray: '#223231',
+      },
+      fontWeight: {
+        normal: 400,
+        bold: 700,
+        extrabold: 800,
+      },
+    },
+  },
+  plugins: [],
+}
