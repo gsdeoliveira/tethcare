b82e4773a0ce3a2dbb00e6806646bfe51594dd40
gsdeoliveira <gabriielsousa2002@gmail.com>
2023-12-18 21:18:46 -0300

    hero padding

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -6,20 +6,20 @@ const Hero = () => {
   return (
     <div className="relative z-20 hero-bg w-full flex flex-col lg:flex-row px-[10px] sm:px-[50px]">
       <div className="w-[2200px] h-[100px] blur-hero absolute z-20 -bottom-[35px] -ml-[100px]"></div>
-      <div className="lg:w-1/2 text-center lg:text-start xl:p-[50px] 2xl:p-[100px] flex flex-col gap-[20px] text-white">
+      <div className="lg:w-1/2 text-center lg:text-start pl-[10px] pt-[10px] sm:pl-[50px] sm:pt-[50px] xl:p-[50px] 2xl:p-[100px] flex flex-col gap-[20px] text-white">
         <img
           src={logo}
           alt="tethcare logo"
           className="w-[180px] self-center lg:self-start"
         />
-        <h1 className="text-[32px] lg:text-[40px] font-bold leading-tight">
+        <h1 className="text-[32px] xl:text-[40px] font-bold leading-tight">
           Sorria com Confiança: Cuidado Odontológico de Qualidade
         </h1>
         <p>
           No TethCare, nossa equipe de especialistas em odontologia está
           comprometida em proporcionar um atendimento odontológico excepcional.
         </p>
-        <Button className="self-center lg:self-start hero-button-gradient w-1/2">
+        <Button className="relative z-30 self-center lg:self-start hero-button-gradient w-1/2">
           Agendar Consulta
         </Button>
       </div>
