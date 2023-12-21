63b9bfddd7f758551c371c3296ed3cb7b7b73410
gsdeoliveira <gabriielsousa2002@gmail.com>
2023-12-20 23:42:38 -0300

    NossoEspaco

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import Hero from './components/sections/Hero'
 import NossaEquipe from './components/sections/NossaEquipe'
 // import NossoEspaco from './components/sections/NossoEspaco'
 import PerguntasFrequentes from './components/sections/PerguntasFrequentes'
+import NossoEspaco from './components/sections/NossoEspaco'
 
 function App() {
   return (
-    <div className="max-w-[1920px] mx-auto font-montserrat overflow-x-hidden bg-[#EEEEEE]">
+    <div className="max-w-[1440px] mx-auto font-montserrat overflow-x-hidden bg-[#EEEEEE]">
       <Hero />
       <Tratamentos />
       <div className="bg-sections">
-        {/* <NossoEspaco /> */}
+        <NossoEspaco />
         <NossaEquipe />
         <PerguntasFrequentes />
       </div>
--- src/components/sections/NossoEspaco.jsx
+++ src/components/sections/NossoEspaco.jsx
@@ -1,30 +1,91 @@
-import { Carousel } from 'react-responsive-carousel'
-import 'react-responsive-carousel/lib/styles/carousel.min.css'
-import './NossoEspaco.css'
+import { useState, useEffect } from 'react'
 import image1 from '../../assets/image-slide-1.jpg'
 import image2 from '../../assets/image-slide-2.jpg'
 import image3 from '../../assets/image-slide-3.jpg'
+import arrow from '../../assets/arrow.svg'
 
 const NossoEspaco = () => {
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const [showSingleImage, setShowSingleImage] = useState(false)
+
+  const images = [
+    image1,
+    image2,
+    image3,
+    image1,
+    image2,
+    image3,
+    // Adicione mais URLs aqui, se desejar
+  ]
+
+  const nextImage = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length)
+  }
+
+  const prevImage = () => {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + images.length) % images.length,
+    )
+  }
+
+  const visibleImages = showSingleImage
+    ? [images[currentIndex]]
+    : [
+        images[(currentIndex - 1 + images.length) % images.length],
+        images[currentIndex],
+        images[(currentIndex + 1) % images.length],
+      ]
+
+  useEffect(() => {
+    const handleResize = () => {
+      const screenWidth = window.innerWidth
+      setShowSingleImage(screenWidth < 1024)
+    }
+
+    // Adiciona um event listener para detectar mudanças no tamanho da tela
+    window.addEventListener('resize', handleResize)
+
+    // Remove o event listener quando o componente é desmontado
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, []) // Apenas executa este efeito uma vez durante a montagem do componente
+
   return (
-    <div className="bg-black text-white flex items-center justify-center p-20 px-32">
-      <Carousel
-        centerMode
-        emulateTouch
-        infiniteLoop
-        selectedItem={1}
-        showThumbs={false}
-      >
-        <div>
-          <img src={image1} alt="Slide 1" />
-        </div>
-        <div>
-          <img src={image2} alt="Slide 2" />
-        </div>
-        <div>
-          <img src={image3} alt="Slide 3" />
-        </div>
-      </Carousel>
+    <div>
+      <h2 className="text-bluePrimary text-center px-[20px]">
+        Conheça Nosso Espaço
+      </h2>
+      <p className="text-darkGray text-center px-[20px] mt-[10px] mb-[50px]">
+        Um Espaço Planejado para Cuidar do Seu Sorriso com Conforto e
+        Tecnologia.
+      </p>
+      <div className="flex items-center justify-center">
+        <img
+          src={arrow}
+          alt="próxima imagem"
+          className="relative z-10 rotate-180 left-20 w-[40px] border-2 border-bluePrimary p-1 rounded-full cursor-pointer"
+          onClick={prevImage}
+        />
+
+        {visibleImages.map((image, index) => (
+          <img
+            key={index}
+            src={image}
+            alt={`Imagem ${index}`}
+            className={`lg:w-[31%] w-full lg:h-[300px] rounded-[5px] m-2 transform ${
+              index === 1 ? 'scale-x-125 opacity-100' : 'scale-75 opacity-80'
+            } transition-all duration-700`}
+          />
+        ))}
+
+        <img
+          src={arrow}
+          alt="próxima imagem"
+          className="relative right-20 w-[40px] border-2 border-bluePrimary p-1 rounded-full cursor-pointer"
+          onClick={nextImage}
+        />
+      </div>
     </div>
   )
 }
